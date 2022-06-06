72760698d2aa16ce6ee4e0a954a384d4791d2844
unknown <gabrielHsantos2018@gmail.com>
2022-06-06 08:19:35 -0300

    commit inicial do projeto

---
+++ src/test/java/br/com/gabriel/apicurso/ApicursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.apicurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
